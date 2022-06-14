458a6e3bca61710f282171b10fed3cf923a83495
nanvon <nanvon@foxmail.com>
2022-06-14 15:29:53 +0800

    [init]引入JSON、初始化

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,40 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { ref } from "vue";
+import dayjs from "dayjs";
+import botData from "@/assets/e.json";
+const startTime = ref(botData.export_data[5687069307].start_at_str);
+const endTime = ref(botData.export_data[5687069307].end_at_str);
+const total = ref(botData.export_data[5687069307].total_export_mblog_count);
+const bot = ref(botData.export_data[5687069307].record_list);
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="header-info">
+    <div>
+      From {{ dayjs(startTime).format("YYYY年MM月DD日") }} to
+      {{ dayjs(endTime).format("YYYY年MM月DD日") }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>Total: {{ total }}</div>
+  </div>
+  <div class="bot-body">
+    <div v-for="item of bot" :key="item.id" class="bot-card">
+      <div v-html="item.text"></div>
+      <div v-html="dayjs(item.created_at).format('YYYY年MM月DD日')"></div>
+    </div>
+  </div>
 </template>
 
-<style>
-@import "./assets/base.css";
-
+<style lang="scss">
 #app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
-
   font-weight: normal;
-}
 
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .bot-body {
+    .bot-card {
+      margin: 10px 0;
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "dayjs/locale/zh-cn";
 
 createApp(App).mount("#app");
