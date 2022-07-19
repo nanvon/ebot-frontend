287e60286d123b3e98b2840a9573f84d14da32cd
nanvon <nanvon@foxmail.com>
2022-07-19 14:40:22 +0800

    add pagination 分页

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,31 @@ const startTime = ref(botData.export_data[5687069307].start_at_str);
 const endTime = ref(botData.export_data[5687069307].end_at_str);
 const total = ref(botData.export_data[5687069307].total_export_mblog_count);
 const bot = ref(botData.export_data[5687069307].record_list);
+const botPageData = ref({});
+botPageData.value = bot.value.slice(0, 10);
+
+const currentPage = ref(1);
+const pageSize = ref(10);
+
+const handleSizeChange = (val) => {
+  botPageData.value = bot.value.slice((currentPage.value - 1) * val, currentPage.value * val);
+}
+const handleCurrentChange = (val) => {
+  botPageData.value = bot.value.slice((val - 1) * pageSize.value, val * pageSize.value);
+}
 </script>
 
 <template>
-  <el-card class="box-card" v-for="item of bot" :key="item.id">
+  <el-pagination
+                 v-model:currentPage="currentPage"
+                 v-model:page-size="pageSize"
+                 :page-sizes="[10, 20, 50, 100]"
+                 layout="jumper, prev, pager, next, sizes, total"
+                 :total="total"
+                 @size-change="handleSizeChange"
+                 @current-change="handleCurrentChange"
+                 class="pagination" />
+  <el-card class="box-card" v-for="item of botPageData" :key="item.id">
     <template #header>
       <div class="card-header">
         <span>{{ dayjs(item.created_at).format('YYYY年MM月DD日') }}</span>
@@ -21,12 +42,23 @@ const bot = ref(botData.export_data[5687069307].record_list);
 
 <style lang="scss">
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  font-weight: normal;
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+  }
+
+  @media (max-width: 700px) {
+    .pagination {
+      flex-direction: column;
+    }
+  }
 
   .box-card {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
     margin-bottom: 10px;
   }
 }
